443c6223c8b2ee4c9ba634b971bb9fccca1f1a1c
Errick Basa-Denis <errickbd@gmail.com>
2024-03-05 08:16:50 -0600

    added planet functionality

--- tailwind-bootstrap-project/src/App.jsx
+++ tailwind-bootstrap-project/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import axios from "axios"
 
 function App() {
   const [fighter, setFighter] = useState([])
+  const [planets, setPlanets] = useState([])
+  const [planetImg, setPlanetImg] = useState('')
 
 
+    useEffect(() => {
+      const fetchPlanets = async () => {
+        try{
+          const responseBg = await axios.get(`https://dragonball-api.com/api/planets?page=1&limit=20`)
+          setPlanets(responseBg.data.items)
+          let data = responseBg.data.items
+          console.log(responseBg)
+
+
+          if (data.length > 0){
+            setPlanetImg(data[0].image)
+          }
+          
+        }
+        catch (error){
+          console.error('Error fetching planets:', error)
+        }
+      }
+      fetchPlanets();
+    }, [])
+
+    
     const getFighter = async(event) => {
       event.preventDefault()
       let form = new FormData(event.target)
@@ -15,9 +39,8 @@ function App() {
         console.log(response.data.items)
         const data = response.data.items
 
-        const responseBg = await axios.get(`https://dragonball-api.com/api/planets?page=1&limit=20`)
-        console.log(responseBg.data.items)
-        const dataBg = responseBg.data.items
+        
+        
 
         for (let fighterData of data){
           if (fighterData.name === formData.fighterName){
@@ -31,22 +54,36 @@ function App() {
       }
     }
 
+    const handlePlanetChange = (event) => {
+      const selectedPlanetName = event.target.value;
+      const selectedPlanet = planets.find(planet => planet.name === selectedPlanetName);
+      setPlanetImg(selectedPlanet.image)
+    }
+
   return (
     <>
-      <h1>DBZ API Fighters</h1>
-      <form onSubmit={getFighter}>
-        <input name='fighterName' placeholder='Enter Z Fighter Name..'></input>
-        <button type='submit'>Search</button>
-      </form>
-      <h2>Character Details:</h2>
-      <p>
-        Name: {fighter.name} <br />
-        Race: {fighter.race} <br />
-        Ki: {fighter.ki} <br />
-      </p>
-      
-      <img src={fighter.image}></img>
-      
+      <body className='planet-image' style={{ backgroundImage: `url(${planetImg})`, backgroundSize: `cover`, margin:'0', padding:`0`}}>
+        <div>
+          <h1>DBZ API Fighters</h1>
+          <form onSubmit={getFighter}>
+            <input name='fighterName' placeholder='Enter Z Fighter Name..'></input>
+            <button type='submit'>Search</button> <br />
+            <label htmlFor='planets'>Choose a planet:</label>
+            <select name='planets' id='planets' onChange={handlePlanetChange}>
+              {planets.map(planet => (
+                <option key={planet.id} value={planet.name}>{planet.name}</option>
+              ))}
+            </select>
+          </form>
+          <h2>Character Details:</h2>
+          <p>
+            Name: {fighter.name} <br />
+            Race: {fighter.race} <br />
+            Ki: {fighter.ki} <br />
+          </p>
+          <img src={fighter.image}></img>
+        </div>
+      </body>
     </>
   )
 }
